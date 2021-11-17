c28083cad3bddd63e3fb3770ad089d08ee0eb67d
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-11-18 00:06:44 +0530

    Added : Welcome message

---
+++ src/main/java/com/bdlz/emppayrolljdbc/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bdlz.emppayrolljdbc;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Employee-Payroll service JDBC program");
+    }
+}
