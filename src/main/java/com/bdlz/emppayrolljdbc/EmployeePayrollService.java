033883ebbb3dec27ebe006f615f0da3aa48a57fe
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-11-18 22:30:55 +0530

    Added: Creating database in Mysql Workbench and connection database i…

--- src/main/java/com/bdlz/emppayrolljdbc/EmployeePayrollService.java
+++ src/main/java/com/bdlz/emppayrolljdbc/EmployeePayrollService.java
@@ -1,7 +1,34 @@
 package com.bdlz.emppayrolljdbc;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeePayrollService {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee-Payroll service JDBC program");
+        EmployeePayrollService service = new EmployeePayrollService();
+        service.getConnection();
+    }
+    private void getConnection() {
+        String url = "jdbc:mysql://localhost:3306/payroll_service";
+        String username = "root";
+        String password = "rootpassword";
+        Connection connection = null;
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection Established");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if(connection != null){
+                try{
+                    connection.close();
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
---
+++ src/test/java/com/bdlz/emppayrolljdbc/EmployeePayrollServiceTest.java
@@ -0,0 +1,13 @@
+package com.bdlz.emppayrolljdbc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
