b8c73d4902a734b176fd42c161723266a438ecf5
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-11-18 23:30:25 +0530

    Added: Ability to retrive employee payroll from the database

--- src/main/java/com/bdlz/emppayrolljdbc/EmployeePayrollService.java
+++ src/main/java/com/bdlz/emppayrolljdbc/EmployeePayrollService.java
@@ -4,13 +4,37 @@ package com.bdlz.emppayrolljdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
 
 public class EmployeePayrollService {
-    public static void main(String[] args) {
-        EmployeePayrollService service = new EmployeePayrollService();
-        service.getConnection();
+    Employee employee = new Employee();
+
+    public static void main(String[] args) throws SQLException {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        employeePayrollService.getConnection();
+        employeePayrollService.insertData();
+        employeePayrollService.retrieveData();
     }
+
+    private void retrieveData() throws SQLException {
+        List<EmployeeInfo> employeeInfoList = employee.retrieveData();
+        System.out.println(employeeInfoList);
+    }
+
+
     private void getConnection() {
         Employee.getConnection();
+    }
+
+    private void insertData() {
+        EmployeeInfo employeeInfo= new EmployeeInfo();
+        employeeInfo.setName("Rama krishna");
+        employeeInfo.setGender('M');
+        employeeInfo.setAddress("Vijayawada");
+        employeeInfo.setPhone("8799877987");
+        employeeInfo.setStartDate(LocalDate.now());
+        Employee employeeRepo=new Employee();
+        employeeRepo.insertData(employeeInfo);
     }
 }
---
+++ src/test/java/com/bdlz/emppayrolljdbc/EmployeePayrollServiceTest.java
@@ -0,0 +1,18 @@
+package com.bdlz.emppayrolljdbc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount(){
+        EmployeePayrollService employeePayrollService=new EmployeePayrollService();
+        List<EmployeeInfo> employeeInfo =employeePayrollService.employee.retrieveData();
+        Assert.assertEquals(7, employeeInfo.size());
+    }
+
+}
