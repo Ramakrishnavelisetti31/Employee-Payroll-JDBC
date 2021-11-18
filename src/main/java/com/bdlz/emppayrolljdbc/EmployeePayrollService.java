58a2f1621ea2b15c04021578f630db4e5ff4010d
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-11-18 23:02:05 +0530

    Added : Welcome message

--- src/main/java/com/bdlz/emppayrolljdbc/EmployeePayrollService.java
+++ src/main/java/com/bdlz/emppayrolljdbc/EmployeePayrollService.java
@@ -7,28 +7,6 @@ import java.sql.SQLException;
 
 public class EmployeePayrollService {
     public static void main(String[] args) {
-        EmployeePayrollService service = new EmployeePayrollService();
-        service.getConnection();
-    }
-    private void getConnection() {
-        String url = "jdbc:mysql://localhost:3306/payroll_service";
-        String username = "root";
-        String password = "rootpassword";
-        Connection connection = null;
-        try {
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-            connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Connection Established");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if(connection != null){
-                try{
-                    connection.close();
-                }catch(SQLException e){
-                    e.printStackTrace();
-                }
-            }
-        }
+        System.out.println("Welcome to the employee_payroll JDBC program");
     }
 }
